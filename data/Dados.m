0ecdac47f93f3322ab5745cb7c147472e1b3a7e6
Ivo Augusto Mudo Moreira <ivomudo@gmail.com>
2026-03-31 17:41:31 -0300

    Adicionado modelos simulink do:

---
+++ data/Dados.m
@@ -0,0 +1,12 @@
+%% Dados 
+
+R = 287;
+V = 1;
+
+A = 1;      %open area of the valve
+cd = 0.8;   %discharge coefficient
+rho = 1;    %density of the fluid, assumed to be constant;
+kappa = 1.4;
+
+Vd = 1;
+N = 4;
